49f00de041a3b59f3e8c9c52c35fe09fb3e39846
jackm <loggerer@gmail.com>
2025-06-09 09:50:29 +0800

    Remove vite adjustment

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   assetsInclude: ['**/*.md'],
-  base: "./",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
